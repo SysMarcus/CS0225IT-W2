01dc62668c9d70cb2b06fbd87c0dece546d021d2
Alessandro Russello <alessandrorussello.work@gmail.com>
2025-04-09 17:50:26 +0200

    Add files via upload

---
+++ w2s2l3/band.py
@@ -0,0 +1,9 @@
+# Richiesta degli input all'utente
+citta = input("Inserisci il nome della tua città di origine:")
+animale = input("Inserisci il nome del tuo animale domestico:")
+
+# Generazione del nome della band
+nome_band = citta + " " + animale
+
+# Output del risultato
+print("Il nome della tua band è:", nome_band)
